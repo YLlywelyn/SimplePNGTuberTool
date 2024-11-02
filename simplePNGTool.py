74b055130bc643504359597494e417ad9c6b27da
Y Llywelyn <ylva.the.defender@gmail.com>
2024-11-02 22:55:29 +0000

    Global hotkeys implemented using bindglobal

--- simplePNGTool.py
+++ simplePNGTool.py
@@ -1,12 +1,16 @@
 import tkinter
 from bindglobal import BindGlobal
 
-def HotKey1(event):
-    onHotkey(1)
-def HotKey2(event):
-    onHotkey(2)
-def HotKey3(event):
-    onHotkey(3)
+def onKeyPress(event):
+    match event.char:
+        case '1':
+            onHotkey(1)
+        case '2':
+            onHotkey(2)
+        case '3':
+            onHotkey(3)
+        case _:
+            pass
 
 def onHotkey(i: int) -> None:
     print(f"switch to preset {i}")
@@ -16,8 +20,6 @@ mainWindow.title("Simple PNG Tool")
 mainWindow.minsize(300, 300)
 
 bg = BindGlobal(widget=mainWindow)
-bg.gbind("<Control-1>", HotKey1)
-bg.gbind("<Control-2>", HotKey2)
-bg.gbind("<Control-3>", HotKey3)
+bg.gbind("<Control-Key>", onKeyPress)
 
 mainWindow.mainloop()
